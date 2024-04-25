2d70950c1437f35d6073a8f7f766ca93c00f3e87
Roman <kovalchuk.roman03@gmail.com>
2024-04-26 01:29:04 +0300

    Fix bug with not updated course dynamic routed pages

--- src/components/course/CourseItem.vue
+++ src/components/course/CourseItem.vue
@@ -6,10 +6,10 @@
            loading="lazy">
       <div class="card-body">
         <div class="card-title h2">
-          <router-link class="stretched-link text-dark text-decoration-none text-nowrap"
-                       :to="'/course/' + course.id">
+          <a class="stretched-link text-dark text-decoration-none text-nowrap"
+                       :href="'/course/' + course.id">
             {{course.name}}
-          </router-link>
+          </a>
         </div>
         <div class="text-muted"><span class="text-nowrap d-inline-block me-4">{{course.duration}}</span>
         </div>
